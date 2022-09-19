62b918e70441a540ea12e9db0b1277601ed7cc94
hero <icraft2170@gmail.com>
2022-09-19 16:39:52 +0900

    feat : #8 주문 생성 프로젝트 생성

---
+++ order-command/src/main/java/com/airmart/ordercommand/OrderCommandApplication.java
@@ -0,0 +1,13 @@
+package com.airmart.ordercommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCommandApplication.class, args);
+	}
+
+}
---
+++ order-command/src/test/java/com/airmart/ordercommand/OrderCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airmart.ordercommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
