8942d813be2b9caefa3b94aea7aca661a853f7ef
hero <icraft2170@gmail.com>
2022-09-20 18:01:31 +0900

    feat : #12 API-Gateway routing 설정 추가

--- order-command/src/main/java/com/airmart/ordercommand/controller/OrderCommandController.java
+++ order-command/src/main/java/com/airmart/ordercommand/controller/OrderCommandController.java
@@ -1,9 +1,11 @@
 package com.airmart.ordercommand.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders/command")
 public class OrderCommandController {
     @GetMapping("/hello")
     public String hello() {
