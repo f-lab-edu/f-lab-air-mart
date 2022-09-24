418109196d86bc9d0a22ccbae050f11379a47852
Hero <81640511+icraft2170@users.noreply.github.com>
2022-09-24 18:43:31 +0900

    feat : #8 ORDER-COMMAND에 Business Error 및 Error Handler 추가

---
+++ order-command/src/main/java/com/airmart/ordercommand/common/exception/BusinessExceptionDictionary.java
@@ -0,0 +1,16 @@
+package com.airmart.ordercommand.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum BusinessExceptionDictionary {
+  UNKNOWN("Unknown", "E000");
+
+  private final String reason;
+  private final String code;
+
+  BusinessExceptionDictionary(final String reason, final String code){
+    this.reason = reason;
+    this.code = code;
+  }
+}
