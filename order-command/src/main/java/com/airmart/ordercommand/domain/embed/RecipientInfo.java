ca9c3ecb516e82d5cb16e7caca9edae316debe98
Hero <81640511+icraft2170@users.noreply.github.com>
2022-09-25 15:31:15 +0900

    feat: #15 VO 에 대한 정적팩토리메서드 생성

--- order-command/src/main/java/com/airmart/ordercommand/domain/embed/RecipientInfo.java
+++ order-command/src/main/java/com/airmart/ordercommand/domain/embed/RecipientInfo.java
@@ -9,4 +9,19 @@ public class RecipientInfo {
     @Embedded
     private Address recipientAddress;
     private String recipientPhoneNumber;
+
+    public RecipientInfo() {}
+
+    private RecipientInfo(String recipientName, String recipientPhoneNumber,
+        Address recipientAddress) {
+        this.recipientName = recipientName;
+        this.recipientAddress = recipientAddress;
+        this.recipientPhoneNumber = recipientPhoneNumber;
+    }
+
+    public static RecipientInfo newInstance(String recipientName, String recipientPhoneNumber,
+        String firstAddress, String secondAddress, String zipcode) {
+        return new RecipientInfo(recipientName, recipientPhoneNumber,
+            Address.newInstance(firstAddress, secondAddress, zipcode));
+    }
 }
--- order-command/src/main/java/com/airmart/ordercommand/domain/embed/SenderInfo.java
+++ order-command/src/main/java/com/airmart/ordercommand/domain/embed/SenderInfo.java
@@ -1,9 +1,22 @@
 package com.airmart.ordercommand.domain.embed;
 
 import javax.persistence.Embeddable;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Embeddable
 public class SenderInfo {
     private String senderName;
     private String senderPhoneNumber;
+
+    private SenderInfo(String senderName, String senderPhoneNumber) {
+        this.senderName = senderName;
+        this.senderPhoneNumber = senderPhoneNumber;
+    }
+
+    public static SenderInfo newInstance(String senderName, String senderPhoneNumber) {
+        return new SenderInfo(senderName, senderPhoneNumber);
+    }
+
+
 }
