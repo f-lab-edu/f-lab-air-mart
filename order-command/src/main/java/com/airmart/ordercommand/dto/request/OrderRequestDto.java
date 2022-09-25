aa146d2fdc655b7e7f5aa5a1209c1fe23bc72275
Hero <81640511+icraft2170@users.noreply.github.com>
2022-09-25 15:37:03 +0900

    feat: #15 OrderRequest 에 필드 추가 및 Entity 변환 로직 추가 - 이 처럼 DTO에 변환 로직을 추가하는 것이 괜찮은지 의문 - DTO 에는 로직이 있어서는 안된다고 들었기 때문에 고민중. 또한 변환로직이라 하지만 사실상 팩토리메서드이고 DTO에 팩토리메서드가 위치한게 캡슐화가 깨진것 처럼 보이기도 함. 그렇다고 RequestDTO를 Order 혹은 GroupOrder에 파라미터로 받아서 변환하는건 나중에 도메인 객체만 다른 모듈에 넣는등에 작업을 하게 된다면 방향성이 양방향으로 갈 것 같다는 생각도 든다. 파라미터를 다 풀어서 쓰기에는 너무 많고 중간에 DTO 하나 더 두기에는 복잡해지는것 같기도 하고 답을 내리기 어려운 문제인듯....

--- order-command/src/main/java/com/airmart/ordercommand/dto/request/OrderRequestDto.java
+++ order-command/src/main/java/com/airmart/ordercommand/dto/request/OrderRequestDto.java
@@ -1,8 +1,12 @@
 package com.airmart.ordercommand.dto.request;
 
-import com.airmart.ordercommand.domain.embed.Address;
-import java.time.Duration;
-import javax.persistence.Embedded;
+import com.airmart.ordercommand.domain.GroupOrder;
+import com.airmart.ordercommand.domain.Order;
+import com.airmart.ordercommand.domain.OrderType;
+import com.airmart.ordercommand.domain.embed.OrderedItem;
+import com.airmart.ordercommand.domain.embed.RecipientInfo;
+import com.airmart.ordercommand.domain.embed.SenderInfo;
+import java.time.LocalDateTime;
 import lombok.Data;
 
 public class OrderRequestDto {
@@ -25,14 +29,8 @@ public class OrderRequestDto {
 
   @Data
   public static class GroupOrderCreateRequest {
-    /**
-     * Format => YYYYMMDDHHMMSS-UUID
-     * - Nullable
-     */
-    private String groupOrderId;
-
     private Long itemId;
-
+    // 구매 수량
     private Long quantity;
 
     private long hours;
@@ -40,12 +38,40 @@ public class OrderRequestDto {
     private SendInfoRequest sendInfo;
 
     private RecipientInfoRequest recipientInfo;
+
+    /**
+     * price 및 minimumQuantity 는 Item을 통해 발생한다.
+     *
+     * @return
+     */
+    public GroupOrder toEntity() {
+      LocalDateTime startDate = LocalDateTime.now();
+      LocalDateTime endDate = startDate.plusHours(hours);
+      GroupOrder groupOrder = GroupOrder.builder()
+          .minimumQuantity(0)
+          .startDate(startDate)
+          .endDate(endDate)
+          .build();
+
+      Order newOrder = new Order(
+          OrderType.GROUP_ORDER
+          , OrderedItem.newInstance(this.quantity, 0L)
+          , recipientInfo.toVO()
+          , sendInfo.toVo()
+      );
+      newOrder.joinGroupOrder(groupOrder);
+      return groupOrder;
+    }
   }
 
   @Data
   public static class SendInfoRequest {
     private String senderName;
     private String senderPhoneNumber;
+
+    public SenderInfo toVo() {
+      return SenderInfo.newInstance(senderName, senderPhoneNumber);
+    }
   }
   @Data
   public static class RecipientInfoRequest {
@@ -54,6 +80,11 @@ public class OrderRequestDto {
     private String secondAddress;
     private String zipCode;
     private String recipientPhoneNumber;
+
+    public RecipientInfo toVO() {
+      return RecipientInfo.newInstance(recipientName, recipientPhoneNumber
+          , firstAddress, secondAddress, zipCode);
+    }
   }
 
 }
