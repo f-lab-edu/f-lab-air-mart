38623cce1ef82c7f20fffb056409a6d3986e902e
Gunjoo Ahn <ahngj96@naver.com>
2022-10-16 21:17:47 +0900

    feat: init project

---
+++ joo-noti-service/src/main/java/com/airmart/joonotiservice/JooNotiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.airmart.joonotiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooNotiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooNotiServiceApplication.class, args);
+    }
+
+}
